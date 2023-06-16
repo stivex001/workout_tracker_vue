a59800c9d5240c65edb9f9b30bfb72cfd3a0e9bb
stivex001 <stephenadeyemo@gmail.com>
2023-06-16 14:57:20 +0100

    feat: added extended configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "at-light-green": "#44ba9f",
+        "at-green": "#286d5d",
+        "light-grey": "#f1f1f1",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins, sans-serif"],
+    },
   },
   plugins: [],
-}
+};
